779352543b817e98dfdd743991d8e8b445101fca
lewiih001 <lewismunyambu@gmail.com>
2022-11-11 00:19:38 +0300

    added const fetch cart

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+
+
+const [cart, setCart] = useState({});
+
+const fetchCart = () => {
+    commerce.cart.retrieve().then((cart) => {
+      setCart(cart);
+    }).catch((error) => {
+      console.log('There was an error fetching the cart', error);
+    });
+  }
